1b016932b113c63d648d9258971193d4f468b20f
Bryton Lacquement <contact@bminusl.xyz>
2022-04-16 11:55:10 +0200

    Extract the tree navigation code into its own mixin

--- main.py
+++ main.py
@@ -8,65 +8,73 @@ class Item(urwid.WidgetWrap):
     signals = ["selected"]
 
     @staticmethod
-    def _markup(entry: os.DirEntry):
+    def _markup(name: str, is_dir: bool):
         # TODO: handle symlinks
-        if entry.is_dir(follow_symlinks=False):
+        if is_dir:
             attr = "folder"
-        elif entry.is_file(follow_symlinks=False):
-            attr = "file"
         else:
-            attr = "unknown"
-        return attr, entry.name
+            attr = "file"
+        # attr = "unknown"
+        return attr, name
 
-    def __init__(self, entry: os.DirEntry):
-        self._entry = entry
-        w = urwid.SelectableIcon(self._markup(entry))
+    def __init__(self, name: str, is_dir: bool):
+        self.name = name
+        self.is_dir = is_dir
+        w = urwid.SelectableIcon(self._markup(name, is_dir))
         super().__init__(w)
 
     def keypress(self, size, key):
         if key in ("l", "enter", "right"):
-            urwid.emit_signal(self, "selected", self._entry)
+            urwid.emit_signal(self, "selected", self)
             return
         return key
 
 
-class BFM(urwid.WidgetWrap):
+class TreeNavigationMixin:
+    def __init__(self, path: str):
+        self.__path = path
+        self._on_path_changed(self.__path)
+
+    def descend(self, into: str):
+        self.__path = os.path.join(self.__path, into)
+        self._on_path_changed(self.__path)
+
+    def ascend(self):
+        self.__path = os.path.split(self.__path)[0]
+        self._on_path_changed(self.__path)
+
+    @staticmethod
+    def __sorting_key(entry):
+        return (not entry.is_dir(follow_symlinks=False), entry.name.lower())
+
+    def scan(self):
+        with os.scandir(self.__path) as it:
+            for entry in sorted(it, key=self.__sorting_key):
+                yield entry.name, entry.is_dir(follow_symlinks=False)
+
+
+class BFM(TreeNavigationMixin, urwid.WidgetWrap):
     def __init__(self, path: str):
         self._w_path = header = urwid.Text("")
         body = urwid.ListBox(urwid.SimpleListWalker([]))
         self._w_contents = body.body
         w = urwid.Frame(body, header)
-        super().__init__(w)
-        self.change_path(path)
 
-    def descend(self, into: str):
-        current = self._w_path.text
-        new_path = os.path.join(current, into)
-        self.change_path(new_path)
+        TreeNavigationMixin.__init__(self, path)
+        urwid.WidgetWrap.__init__(self, w)
 
-    def ascend(self):
-        current = self._w_path.text
-        new_path = os.path.split(current)[0]
-        self.change_path(new_path)
-
-    def change_path(self, new_path: str):
-        def sorting_key(entry):
-            return (not entry.is_dir(follow_symlinks=False), entry.name.lower())
-
-        def on_item_selected(entry):
-            if entry.is_dir(follow_symlinks=False):
-                self.descend(entry.name)
-            elif entry.is_file(follow_symlinks=False):
-                # TODO:
-                pass
+    def _on_path_changed(self, new_path: str):
+        def on_item_selected(item: Item):
+            if item.is_dir:
+                self.descend(item.name)
 
         self._w_path.set_text(("path", new_path))
         self._w_contents.clear()
-        with os.scandir(new_path) as it:
-            for entry in sorted(it, key=sorting_key):
-                item = Item(entry)
-                self._w_contents.append(item)
-                urwid.connect_signal(item, "selected", on_item_selected)
+
+        for args in self.scan():
+            item = Item(*args)
+            self._w_contents.append(item)
+            urwid.connect_signal(item, "selected", on_item_selected)
 
     def keypress(self, size, key):
         key_to_propagate = key
