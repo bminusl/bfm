5d4676f230faa02b90dff7498de902b0e1f2600c
Bryton Lacquement <contact@bminusl.xyz>
2022-04-17 14:48:42 +0200

    Extract the listbox features into ItemList (wip)
    The preview pane is broken. WIP.

--- bfm/widgets.py
+++ bfm/widgets.py
@@ -36,39 +36,57 @@ class Item(urwid.Text):
         return key
 
 
+class ItemList(urwid.ListBox):
+    def __init__(self, body):
+        super().__init__(urwid.SimpleListWalker(body))
+
+    def keypress(self, size, key):
+        key_to_propagate = key
+        if key in ("j", "down"):
+            key_to_propagate = "down"
+        elif key in ("k", "up"):
+            key_to_propagate = "up"
+        return super().keypress(size, key_to_propagate)
+
+
 class BFM(TreeNavigationMixin, urwid.WidgetWrap):
     def __init__(self, path: str):
         w_path = urwid.Text("")
         w_command = urwid.Text("")
         w_header = urwid.Pile([w_path, w_command])
 
-        w_empty = urwid.SolidFill("x")  # XXX: whatever
-        w_items = urwid.ListBox(urwid.SimpleListWalker([]))
+        # IMPORTANT:
+        # The original_widget parameter passed to urwid.WidgetPlaceholder MUST
+        # be selectable.
+        # If not, it will not work properly when we want to replace it with a
+        # selectable one, i.e. `placeholder.original_widget = new_widget`.
+        # This seems to be a bug/non-documented behaviour. urwid.SelectableIcon
+        # is thus used as a workaround/trick. See this discussion[0].
+        # [0]: https://gitter.im/urwid/community?at=5f90305cea6bfb0a9a4bd0ac
+        w_empty = urwid.Filler(urwid.SelectableIcon(""), valign="top")
+        w_item_list_placeholder = urwid.WidgetPlaceholder(w_empty)
         w_preview_placeholder = urwid.WidgetPlaceholder(w_empty)
-        w_body = urwid.Columns([w_items, w_preview_placeholder])
+        w_body = urwid.Columns([w_item_list_placeholder, w_preview_placeholder])
 
         w = urwid.Frame(w_body, w_header)
 
         self._w_path = weakref.proxy(w_path)
         self._w_command = weakref.proxy(w_command)
-        self._w_items = weakref.proxy(w_items)
-        self._w_items_contents = weakref.proxy(w_items.body)
+        self._w_item_list_placeholder = weakref.proxy(w_item_list_placeholder)
         self._w_preview_placeholder = weakref.proxy(w_preview_placeholder)
 
         TreeNavigationMixin.__init__(self, path)
         urwid.WidgetWrap.__init__(self, w)
 
-        urwid.connect_signal(
-            w_items.body, "modified", self._on_items_contents_modified
-        )
-        self._on_items_contents_modified()
+        # urwid.connect_signal(w_item_list.body, "modified", self._on_tmp_foo)
+        # self._on_tmp_foo()
 
-    def _on_items_contents_modified(self):
-        if self._w_items_contents:
-            item = self._w_items.get_focus()[0]
+    def _on_tmp_foo(self):
+        if self._w_item_list.body:
+            item = self._w_item_list.get_focus()[0]
             if item.entry.is_dir(follow_symlinks=False):
                 contents = [Item(e) for e in self.scanpath(item.entry.path)]
-                w_new = urwid.ListBox(urwid.SimpleListWalker(contents))
+                w_new = ItemList(contents)
             else:
                 # TODO: catch errors, large files, etc
                 with open(item.entry.path) as f:
@@ -87,12 +105,14 @@ class BFM(TreeNavigationMixin, urwid.WidgetWrap):
 
     def _on_path_changed(self, new_path: str):
         self._w_path.set_text(("path", new_path))
-        self._w_items_contents.clear()
 
-        for entry in self.scanpath():
-            item = Item(entry)
-            self._w_items_contents.append(item)
+        items = [Item(entry) for entry in self.scanpath()]
+        w_item_list = ItemList(items)
+
+        for item in w_item_list.body:
             urwid.connect_signal(item, "selected", self._on_item_selected)
+
+        self._w_item_list_placeholder.original_widget = w_item_list
 
     def edit_file(self, path: str):
         from . import loop
@@ -103,12 +123,7 @@ class BFM(TreeNavigationMixin, urwid.WidgetWrap):
         loop.screen.start()
 
     def keypress(self, size, key):
-        key_to_propagate = key
         if key in ("h", "backspace", "left"):
             self.ascend()
             return
-        elif key in ("j", "down"):
-            key_to_propagate = "down"
-        elif key in ("k", "up"):
-            key_to_propagate = "up"
-        return super().keypress(size, key_to_propagate)
+        return super().keypress(size, key)
