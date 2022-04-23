79de86f416f761a4833d5c5e0db5c08e6223995d
Bryton Lacquement <contact@bminusl.xyz>
2022-04-23 11:58:32 +0200

    Directly use the FolderWidget instead of a placeholder

--- bfm/widgets.py
+++ bfm/widgets.py
@@ -77,8 +77,13 @@ class FolderWidget(urwid.ListBox):
         aliases={"<down>": "j", "<up>": "k"},
     )
 
-    def __init__(self, items):
+    def __init__(self, items=[]):
         super().__init__(urwid.SimpleListWalker(items))
+
+        self.append = self.body.append
+        self.extend = self.body.extend
+        self.clear = self.body.clear
+
         urwid.connect_signal(self.body, "modified", self._on_body_modified)
 
     def get_focused_item(self) -> ItemWidget:
@@ -154,31 +159,18 @@ class BFMWidget(
         w_command = CommandWidget()
         w_header = urwid.Pile([w_path, w_command])
 
-        # IMPORTANT:
-        # The original_widget parameter passed to urwid.WidgetPlaceholder MUST
-        # be selectable.
-        # If not, it will not work properly when we want to replace it with a
-        # selectable one, i.e. `placeholder.original_widget = new_widget`.
-        # This seems to be a bug/non-documented behaviour. urwid.SelectableIcon
-        # is thus used as a workaround/trick. See this discussion[0].
-        # [0]: https://gitter.im/urwid/community?at=5f90305cea6bfb0a9a4bd0ac
-        w_empty = urwid.Filler(urwid.SelectableIcon(""), valign="top")
-        w_folder_placeholder = urwid.WidgetPlaceholder(w_empty)
+        w_folder = FolderWidget()
         w_preview = ANSIWidget()
-        w_body = urwid.Columns([w_folder_placeholder, w_preview], dividechars=1)
+        w_body = urwid.Columns([w_folder, w_preview], dividechars=1)
 
         w_extra = urwid.Text("")
 
         w_frame = FocusableFrameWidget(w_header, w_body, w_extra)
         w = urwid.Padding(w_frame, left=1, right=1)
 
-        # Cache FolderWidget instances when navigating the tree to reuse them
-        # later
-        self._folders = {}
-
         self._w_path = weakref.proxy(w_path)
         self._w_command = weakref.proxy(w_command)
-        self._w_folder_placeholder = weakref.proxy(w_folder_placeholder)
+        self._w_folder = weakref.proxy(w_folder)
         self._w_preview = weakref.proxy(w_preview)
         self._w_extra = weakref.proxy(w_extra)
         self._w_frame = weakref.proxy(w_frame)
@@ -188,26 +180,12 @@ class BFMWidget(
         self._preview_pipe_fd = loop.watch_pipe(self._add_to_preview)
 
         urwid.connect_signal(w_command, "validated", self._on_command_validated)
+        urwid.connect_signal(
+            w_folder, "focus_changed", self._on_folder_focus_changed
+        )
 
         TreeNavigationMixin.__init__(self, path)
         urwid.WidgetWrap.__init__(self, w)
-
-    def ascend(self, *args, **kwargs):
-        from_ = super().ascend(*args, **kwargs)
-        # Patch to focus the correct item when ascending
-        folder = self._w_folder_placeholder.original_widget
-        for i, item in enumerate(folder.body):
-            if item.entry.name == from_:
-                folder.set_focus(i)
-                break
-        return from_
-
-    def create_folder(self, path: str) -> FolderWidget:
-        w = FolderWidget([ItemWidget(entry) for entry in self.scanpath(path)])
-        for item in w.body:
-            urwid.connect_signal(item, "selected", self._on_item_selected)
-        urwid.connect_signal(w, "focus_changed", self._on_folder_focus_changed)
-        return w
 
     def edit_file(self, path: str):
         from . import loop
@@ -274,10 +252,11 @@ class BFMWidget(
             self.edit_file(item.entry.path)
 
     def _on_path_changed(self, new_path: str):
+        folder = self._w_folder
         self._w_path.set_text(("path", new_path))
-        if new_path in self._folders:
-            folder = self._folders[new_path]
-        else:
-            self._folders[new_path] = folder = self.create_folder(new_path)
-        self._w_folder_placeholder.original_widget = folder
-        self._on_folder_focus_changed(folder.get_focused_item())  # Trick
+
+        folder.clear()
+        for entry in self.scanpath(new_path):
+            item = ItemWidget(entry)
+            folder.append(item)
+            urwid.connect_signal(item, "selected", self._on_item_selected)
